bdbe666a8006ab03b1ff6d4d7d4ab2b2155bfbe2
Riley Watts <rileywatts.yes@gmail.com>
2017-10-20 12:20:28 -0700

    initial rework layout set

---
+++ app/src/main/java/com/gritstaffing/heyjoe/Landing.java
@@ -0,0 +1,13 @@
+package com.gritstaffing.heyjoe;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Landing extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_landing);
+    }
+}
